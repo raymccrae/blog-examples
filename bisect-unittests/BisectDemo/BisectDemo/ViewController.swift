070ba42ae77693948a11a26e6c1b74de9577fe2f
Raymond McCrae <raymccrae@yahoo.com>
2018-03-20 19:51:17 +0000

    Add slider to UI

--- bisect-unittests/BisectDemo/BisectDemo/ViewController.swift
+++ bisect-unittests/BisectDemo/BisectDemo/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let average = Utilities.average(values: [])
-        self.label.text = "\(average)"
+        updateLabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderValudChanged(_ sender: Any) {
+        updateLabel()
+    }
 
+    func updateLabel() {
+        let value = Int(slider.value)
+        let average = Utilities.average(values: Array(0...value))
+        label.text = "Avergae: \(average)"
+    }
 }
 
