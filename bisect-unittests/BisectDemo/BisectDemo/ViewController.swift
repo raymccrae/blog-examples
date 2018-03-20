bf4f2f3dd8f5c0241427131e3e1b2c0df9fba91f
Raymond McCrae <raymccrae@yahoo.com>
2018-03-20 19:52:54 +0000

    Remove didReceiveMemoryWarning from ViewController

--- bisect-unittests/BisectDemo/BisectDemo/ViewController.swift
+++ bisect-unittests/BisectDemo/BisectDemo/ViewController.swift
@@ -20,11 +20,6 @@ class ViewController: UIViewController {
         self.label.text = "\(average)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
